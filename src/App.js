1bb428c93e84b730c6432069168a59fd97ce2385
kaye kinser <kaly@kinser.us>
2023-03-18 05:21:28 -0500

    Image api integration

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       <main>
         <Dictionary defaultKeyword="forest" />
       </main>
-      <footer className="App-footer">Developed by Kaye Kinser, open-sourced on <a href="https://github.com/KayeBK/dictionary-react" rel="noreferrer">GitHub</a> and hosted on <a href="https://precious-alpaca-b2f149.netlify.app/" rel="noreferrer">Netlify</a>.</footer>
+      <footer className="App-footer">Developed by Kaye Kinser, open-sourced on <a href="https://github.com/KayeBK/dictionary-react" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://precious-alpaca-b2f149.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>.</footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import SearchResults from "./SearchResults"
+import Photos from "./Photos"
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
     let [loaded, setLoaded] = useState(false);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         setResults(response.data);
+    }
+
+    function handleImageResponse(response) {
+        setPhotos(response.data);
     }
 
     function search() {
         let apiKey = `0e89d84fo638t41edd00d5b9415a6a37`
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-        axios.get(apiUrl).then(handleResponse);
+        let apiUrlDictionary = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrlDictionary).then(handleDictionaryResponse);
+        let apiUrlImage = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+        axios.get(apiUrlImage).then(handleImageResponse);
     }
 
     function handleSubmit(event) {
@@ -43,6 +51,7 @@ if (loaded) {
             </form>
             </section>
             <SearchResults data={results} />
+            <Photos data={photos} />
         </div>
     );
 } else {
