1749be456f6b3705ae1c6b81e753c51dd0e5418b
kaye kinser <kaly@kinser.us>
2023-03-18 00:11:20 -0500

    basic search engine added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">Hello</header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">developed by kaye</footer>
+      </div>
     </div>
   );
 }
