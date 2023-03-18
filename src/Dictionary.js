5779a6fa8bc8c7444288d804ea0f0ada5bf046ea
kaye kinser <kaly@kinser.us>
2023-03-18 01:15:19 -0500

    Added definitions and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import SearchResults from "./SearchResults"
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data);
     }
 
     function search(event) {
         event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        console.log(apiUrl);
+        let apiKey = `0e89d84fo638t41edd00d5b9415a6a37`
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -27,6 +29,7 @@ export default function Dictionary() {
                 <input type="search" placeholder="Enter a word.." onChange={handleKeywordChange} />
                 <input type="submit" value="Search" />
             </form>
+            <SearchResults data={results} />
         </div>
     );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meaning(props) {
+    console.log(props.meaning);
+    return (
+        <div className="Meaning">
+        <h5>{props.meaning.partOfSpeech}</h5>
+        <p>{props.meaning.definition}
+        <br />
+        <em>{props.meaning.example}</em></p>
+        </div>
+    );
+}
